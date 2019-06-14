b7c0321d3b60c1f3bdef96086d6f833374e62b08
ShubhamLaad <shubhamlaad666@gmail.com>
2019-06-14 11:19:04 +0530

    Start home page

--- App.js
+++ App.js
@@ -1,19 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const food = require('./assets/icon.png');
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const FOOD_LIST = [{
+  id: 1,
+  foodName: 'Sabji',
+  foodImage: food,
+}, {
+  id: 2,
+  foodName: 'rooti',
+  foodImage: food,
+}];
+export default class App extends React.PureComponent {
+
+  renderFoodList = ({ item }) => (
+    <View>
+      <Text>{item.foodName}</Text>
+      <Image source={food} style={styles.foodImage} />
     </View>
   );
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.row}>
+            <Icon
+              name='glass' />
+            <Text>Delhi</Text>
+          </View>
+          <View style={styles.row}>
+            <Icon name='search' size={20} />
+            <Icon name='user' size={20} style={styles.userIcon} />
+          </View>
+        </View>
+
+        <ScrollView>
+          <FlatList
+            data={FOOD_LIST}
+            renderItem={this.renderFoodList}
+          />
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
   },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  icon: {
+    fontSize: 15,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  userIcon: {
+    marginLeft: 10,
+  },
+  foodImage: {
+    height: 50,
+    width: 50,
+  }
+
 });
